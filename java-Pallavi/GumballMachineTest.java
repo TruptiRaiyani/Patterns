66d49fc839877f82f781329287724aa90b6dd4be
Pallavi <pallavikhandekar212@gmail.com>
2013-09-26 18:21:16 -0700

    added test cases

--- java-Pallavi/GumballMachineTest.java
+++ java-Pallavi/GumballMachineTest.java
@@ -57,6 +57,77 @@ public class GumballMachineTest
         gumballM1.turnCrank();
         assertEquals(true, gumballM1.isGumballInSlot());
     }
+
+    @Test
+    public void testQDDNHappy()
+    {
+        gumballM1.insertQuarter();
+        gumballM1.insertDime();
+        gumballM1.insertDime();
+        gumballM1.insertNickel();
+        gumballM1.turnCrank();
+        assertEquals(true, gumballM1.isGumballInSlot());
+    }
+
+    @Test
+    public void testQDNNNHappy()
+    {
+        gumballM1.insertQuarter();
+        gumballM1.insertDime();
+        gumballM1.insertNickel();
+        gumballM1.insertNickel();
+        gumballM1.insertNickel();
+        gumballM1.turnCrank();
+        assertEquals(true, gumballM1.isGumballInSlot());
+    }
+
+    @Test
+    public void testQNNNNNHappy()
+    {
+        gumballM1.insertQuarter();
+        gumballM1.insertNickel();
+        gumballM1.insertNickel();
+        gumballM1.insertNickel();
+        gumballM1.insertNickel();
+        gumballM1.insertNickel();
+        gumballM1.turnCrank();
+        assertEquals(true, gumballM1.isGumballInSlot());
+    }
+
+    
+
+    @Test
+    public void testNoFiftyCentsToHasFiftyCentsStateTransition()
+    {
+        gumballM1.insertQuarter();
+        gumballM1.insertQuarter();
+        boolean isHasFiftyCentsState = false;
+        if (gumballM1.getState() instanceof HasFiftyCentsState)
+            isHasFiftyCentsState = true;
+        
+        assertEquals(true, isHasFiftyCentsState);
+    }
+    
+    @Test
+    public void testHasFiftyCentsToSoldState()
+    {
+        gumballM1.insertQuarter();
+        gumballM1.insertQuarter();
+        gumballM1.turnCrank();
+        boolean isSoldState = false;
+        if (gumballM1.getState() instanceof SoldState)
+            isSoldState = true;
+        
+        assertEquals(true, isSoldState);
+    
+    }
+    
+   
 }
+
+
+
+
+
 
 
