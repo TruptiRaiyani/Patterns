18fe441565cfedaba897bfaa4061f4a394fa5880
Pallavi <pallavikhandekar212@gmail.com>
2013-10-06 13:21:34 -0700

    added background, hen and plank

---
+++ EggDrop/EggDrop/Farm.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Farm here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Farm extends World
+{
+
+    /**
+     * Constructor for objects of class Farm.
+     * 
+     */
+    public Farm()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 500, 1); 
+       addObjectsToFarm();
+    }
+    
+    void addObjectsToFarm()
+    {
+      WoodenPlank woodenplank = new WoodenPlank();
+      addObject(woodenplank, 400, 150);
+      
+      Hen hen = new Hen();
+      addObject(hen, 400, 78);
+    }
+}
---
+++ EggDrop/EggDrop/WoodenPlank.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WoodenPlank here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WoodenPlank extends Actor
+{
+    public WoodenPlank()
+    {
+        GreenfootImage image = getImage() ;
+        image.scale( 800, 50 ) ; 
+    }
+    /**
+     * Act - do whatever the WoodenPlank wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
