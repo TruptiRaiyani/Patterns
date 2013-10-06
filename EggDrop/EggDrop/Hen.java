2d15270ed8d9394915b8e9181bccab285bf2ec33
Pallavi <pallavikhandekar212@gmail.com>
2013-10-06 15:10:24 -0700

    random movement

--- EggDrop/EggDrop/Hen.java
+++ EggDrop/EggDrop/Hen.java
@@ -13,6 +13,7 @@ public class Hen extends Actor
     {
         GreenfootImage image = getImage() ;
         image.scale( 80, 120) ; 
+        getImage().mirrorHorizontally();
     }
     /**
      * Act - do whatever the Chicken wants to do. This method is called whenever
@@ -21,5 +22,23 @@ public class Hen extends Actor
     public void act() 
     {
         // Add your action code here.
-    }    
+        move(4);
+        int random = Greenfoot.getRandomNumber(5000);
+        if((random>200 & random<300) || atWorldEdge())
+        {
+            turn(180);
+            getImage().mirrorVertically();
+            move(4);
+        }
+    }  
+    
+    private boolean atWorldEdge()  
+    {  
+        if(getX() < 10 || getX() > getWorld().getWidth() - 10)  
+            return true;  
+        if(getY() < 10 || getY() > getWorld().getHeight() - 10)  
+            return true;  
+        else  
+            return false;  
+    }  
 }
