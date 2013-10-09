649b97408680009368180492d13ab32c264ac06e
Namitha Shetty <namithashetty@Kforces-MacBook-Pro.local>
2013-10-09 00:52:08 -0700

    EggCrackSoundAdded

--- EggDrop/EggDrop/Egg.java
+++ EggDrop/EggDrop/Egg.java
@@ -12,6 +12,8 @@ public class Egg extends Actor
      * Act - do whatever the Egg wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+     GreenfootSound sound = new GreenfootSound("Egg Crack.wav"); 
+    
     public void act() 
     {
        Farm farm =  (Farm)getWorld();
@@ -29,12 +31,16 @@ public class Egg extends Actor
         }
         else if(((Farm)getWorld()).atWorldEdge(this))
         {
-            
+            sound.play();
+            sound.setVolume(85);
             setImage("egg-break.gif");
             getImage().scale( 50,50) ;
             setLocation(getX(), ((Farm)getWorld()).getHeight()-25);
+
             if(Greenfoot.getRandomNumber(50) == 0) {
+                sound.setVolume(0);
                 world.removeObject(this);
+
             }
     }    
 }
