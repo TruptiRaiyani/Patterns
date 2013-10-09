027757d7dff514028f4402dc336c1448cdc77bca
Pratiksha Pai <pk@Pratikshas-MacBook-Air.local>
2013-10-09 12:15:16 -0700

    <Counter for gold egg>

--- EggDrop/EggDrop/Egg.java
+++ EggDrop/EggDrop/Egg.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Egg extends Actor
+public abstract class Egg extends Actor
 {
     
     
@@ -18,39 +18,9 @@ public class Egg extends Actor
     GreenfootSound sound = new GreenfootSound("Egg Crack.wav"); 
     public void act() 
     {
-       Farm farm =  (Farm)getWorld();
-        int yOfBasket = farm.getBasket().getY();
-        int xOfBasket = farm.getBasket().getX();
-        
-        setRotation(90);
-        move(3);
-         World world ;
-         world = getWorld();
-    
-        if(isTouching(Basket.class) && (yOfBasket==getY()) )
-        {
-            
-            Farm.c.add(1);
-            world.removeObject(this);   
-             
-        } 
-        else if(((Farm)getWorld()).atWorldEdge(this))
-        {
-            sound.play();
-            sound.setVolume(85);
-            setImage("egg-break.gif");
-            getImage().scale( 50,50) ;
-            setLocation(getX(), ((Farm)getWorld()).getHeight()-25);
-
-            if(Greenfoot.getRandomNumber(50) == 0) {
-                sound.setVolume(0);
-                world.removeObject(this);
-
-            }
-    } 
-    
-    
+      actEgg();
    }
-
+   
+    public abstract void actEgg();
 
 }
