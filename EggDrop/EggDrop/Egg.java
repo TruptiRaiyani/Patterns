72bbceff6e7f18c6fa6fdc7c8d853b379552a9a6
Pallavi <pallavikhandekar212@gmail.com>
2013-11-19 11:28:18 -0800

    modified notifyLifeObserver

--- EggDrop/EggDrop/Egg.java
+++ EggDrop/EggDrop/Egg.java
@@ -93,7 +93,7 @@ public abstract class Egg extends Actor implements Subject
     
      public void setState(String success){
         this.success = success;
-        notifyObserver();
+        notifyLifeObserver();
     }
        
     public String getState(){
